095fba744b2785d67ebec1b5cee3a3db94dcb23b
ycqy123 <liuyang@keget.cn>
2020-03-02 16:08:05 +0800

    Create user.js

---
+++ src/api/user.js
@@ -0,0 +1,23 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/users/login/',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo() {
+  return request({
+    url: '/users/current/',
+    method: 'get'
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/vue-admin-template/user/logout',
+    method: 'post'
+  })
+}
